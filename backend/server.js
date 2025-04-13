51ad34dc2d5edffcadd87fd740d1908bac001da5
S-Amna-Amir <i221142@nu.edu.pk>
2025-04-13 14:47:06 +0500

    Initial commit: basic setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('MERN App is running!');
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log("MongoDB connected"))
+.catch(err => console.log('Error connecting to MongoDB:', err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
